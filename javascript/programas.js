019e51ec38dbe59d38d9e67afe4f2558da8cc868
michel <u20231211735sco.edu.co>
2024-09-10 08:11:16 -0500

    pagina

---
+++ javascript/programas.js
@@ -0,0 +1,19 @@
+
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+};
